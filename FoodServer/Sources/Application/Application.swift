ac2c5ba41cbd70bde3ab789949439f7fe8c76c30
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-30 11:37:52 +0900

    Rewrite to use PostgreSQL

--- FoodServer/Sources/Application/Application.swift
+++ FoodServer/Sources/Application/Application.swift
@@ -5,15 +5,26 @@ import Configuration
 import CloudEnvironment
 import KituraContracts
 import Health
+import SwiftKueryORM
+import SwiftKueryPostgreSQL
 
 public let projectPath = ConfigurationManager.BasePath.project.path
 public let health = Health()
 
+extension Meal: Model {
+    static var idColumnName = "name"
+}
+
+class Persistence {
+    static func setUp() {
+        let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("FoodDatabase")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+        Database.default = Database(pool)
+    }
+}
+
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
-    
-    private var mealStore: [String: Meal] = [:]
 
     public init() throws {
         // Run the metrics initializer
@@ -27,21 +38,31 @@ public class App {
         router.post("/meals", handler: storeHandler)
         router.get("/meals", handler: loadHandler)
         router.get("/summary", handler: summaryHandler)
+        
+        Persistence.setUp()
+        do {
+            try Meal.createTableSync()
+        } catch let error {
+            print(error)
+        }
     }
     
-    func storeHandler(meal: Meal, completion: (Meal?, RequestError?) -> Void) {
-        mealStore[meal.name] = meal
-        completion(mealStore[meal.name], nil)
+    func storeHandler(meal: Meal, completion: @escaping (Meal?, RequestError?) -> Void) {
+        meal.save(completion)
     }
     
-    func loadHandler(completion: ([Meal]?, RequestError?) -> Void) {
-        let meals: [Meal] = self.mealStore.map({ $0.value })
-        completion(meals, nil)
+    func loadHandler(completion: @escaping ([Meal]?, RequestError?) -> Void) {
+        Meal.findAll(completion)
     }
     
-    func summaryHandler(completion: (Summary?, RequestError?) -> Void) {
-        let summary: Summary = Summary(self.mealStore)
-        completion(summary, nil)
+    func summaryHandler(completion: @escaping (Summary?, RequestError?) -> Void) {
+        Meal.findAll { meals, error in
+            guard let meals = meals else {
+                completion(nil, .internalServerError)
+                return
+            }
+        completion(Summary(meals), nil)
+        }
     }
 
     public func run() throws {
